f1df72e70bacf54edbc0eb6e7230f2c1049697a4
Ashkon Honardoost <ashkon91@gmail.com>
2017-01-19 02:08:37 -0800

    added js

---
+++ static/js/introHCI.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+	$("a.thumbnail").click(projectClick);
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	console.log("Project clicked!");
+	// prevent the page from reloading
+	e.preventDefault();
+
+	// $(this) refers to the object that called it in event handling
+	$(this).css("background-color", "#7fff00");
+
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+}
